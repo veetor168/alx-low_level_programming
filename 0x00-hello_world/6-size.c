834288d5f42feb6b4bb4eb1a9837829bff430fc4
veetor168 <ihieprecious@gmail.com>
2022-09-08 18:04:48 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 
+
+
 /**
 
- * main - Entry point
+* main - print the string in the put function
 
- * 
+*
 
- * Return:  Always 0 (Success)
+* Description: using the main function
 
- */
+* this program prints "Programming is like building a multilingual puzzle
+
+* Return: 0
+
+*/
 
 int main(void)
 
 {
 
-printf(“Size of a char : %d byte(s)\n” , sizeof(char));
+	puts("\"Programming is like building a multilingual puzzle");
 
-printf(“Size of an int : %d byte(s)\n” , sizeof(int));
-
-printf(“Size of a long int : %d byte(s)\n” , sizeof(long int));
-
-printf(“Size of a long long int : %d byte(s)\n” , sizeof(long long int));
-
-printf(“Size of a float : %d byte(s)\n” , sizeof(float));
-
-return (0);
+	return (0);
 
 }
 
