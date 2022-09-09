5d8be1a2ddb0280e37b82cdf5c122af7ae5aa00f
veetor168 <ihieprecious@gmail.com>
2022-09-08 17:16:29 -0700

    printing

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+
+ * Description: main - Prints out a string to stdout. 
+
+ * Return: 0 if success.
+
+ */
+
+int main(void)
+
+{
+
+puts("with proper grammar, but the outcome is a piece of art, \n”);
+
+return (0);
+
+}
+
