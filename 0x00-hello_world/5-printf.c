86eeaa8ba685a5cd5d011d2f0fa96b25a699ca2d
veetor168 <ihieprecious@gmail.com>
2022-09-09 02:27:03 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,20 @@
-#include <stdio.h>
-/**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
- */
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piecec of art,\n");
-return (0);
+#include <stdio.h>                                                                                                                                       
+
+/**                                                                                                                                                      
+
+ * Description: main - Prints out a string to stdout.                                                                                                    
+
+ * Return: 0 if success.                                                                                                                                 
+
+ */                                                                                                                                                      
+
+int main(void)                                                                                                                                           
+
+{                                                                                                                                                        
+
+printf("with proper grammar, but the outcome is a piece of art,\n");                                                                                    
+
+return (0);                                                                                                                                              
+
 }
+
