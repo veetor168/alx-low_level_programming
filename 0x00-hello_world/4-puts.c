74c4cf91c712f8ab7dc7628cee2d12e6cc6024c4
veetor168 <ihieprecious@gmail.com>
2022-09-08 17:09:36 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
-
 /**
-
-* main - Printing
-
-*
-
-* Return: Always 0 (Successful)
-
-*/
-
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if successful.
+ */
 int main(void)
-
 {
-
-puts("\"Programming is like building a multilingual
-
-puzzle");
-
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-
 }
-
